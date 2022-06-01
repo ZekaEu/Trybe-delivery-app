49ba9212dee20b1a46bfc6fc85269798158483a9
Thiago <thiago_ssfr@hotmail.com>
2022-06-01 15:19:55 -0300

    setup sequelize

---
+++ back-end/src/database/models/Sale.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const Sale = sequelize.define('Sale', {
+    id: { allowNull: false, autoIncrement: true, type: DataTypes.INTEGER, primaryKey: true },
+    userId: { allowNull: false, type: DataTypes.INTEGER },
+    sellerId: { allowNull: false, type: DataTypes.INTEGER },
+    totalPrice: { allowNull: false, type: DataTypes.DECIMAL },
+    deliveryAddress: { allowNull: false, type: DataTypes.STRING },
+    deliveryNumber: { allowNull: false, type: DataTypes.INTEGER },
+    saleDate: { allowNull: false, type: DataTypes.DATE },
+    status: { allowNull: false, type: DataTypes.STRING },
+  },
+  {
+    timestamps: false,
+    tableName: 'sales',
+    underscored: true,  
+  });
+
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User, { foreignKey: 'userId', as: 'users' });
+    Sale.belongsTo(models.User, { foreignKey: 'sellerId', as: 'users' });
+  };
+
+  return Sale;
+};
