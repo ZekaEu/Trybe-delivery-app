4098709cf4fdade2d1039c50f19a43a5e4e49b0f
Arthur Palomo <arth_nf@hotmail.com>
2022-06-02 18:08:52 -0300

    http-status-codes added

--- back-end/src/api/services/User.js
+++ back-end/src/api/services/User.js
@@ -1,5 +1,5 @@
 const md5 = require('md5');
-// const { StatusCodes } = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const { User } = require('../../database/models');
 const { generateToken } = require('./auth');
 
@@ -8,7 +8,7 @@ const passwordValid = (user, password) => md5(password) === user.password;
 const findUser = async (email, password) => {
   const user = await User.findOne({ where: { email } });
   if (!user || !passwordValid(user, password)) { 
-    return { code: 400, data: { message: 'Invalid email or password' } };
+    return { code: StatusCodes.BAD_REQUEST, data: { message: 'Invalid email or password' } };
   }
   const token = generateToken(user);
   const safeUser = {
@@ -17,7 +17,7 @@ const findUser = async (email, password) => {
     role: user.role,
     token,
   };
-  return { code: 200, data: safeUser };
+  return { code: StatusCodes.OK, data: safeUser };
 };
 
 module.exports = { findUser };
