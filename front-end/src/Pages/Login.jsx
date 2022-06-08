89374a368f419b4a5b5a776f478a8fdfe6abde31
Joao-Ortega <joao_ortega17@hotmail.com>
2022-06-08 16:44:48 -0300

    Adding product quantity change

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -9,9 +9,9 @@ export default function Login() {
         <div className="row d-flex align-items-center justify-content-around">
           <div className="col-md-8 col-lg-7 col-xl-6 img-container">
             <img
-              src="https://stormotion.io/blog/content/images/2020/12/cover-on-demand-delivery.jpg"
+              src=""
               className="img-fluid"
-              alt="Phone"
+              alt="Delivery"
             />
           </div>
           <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
