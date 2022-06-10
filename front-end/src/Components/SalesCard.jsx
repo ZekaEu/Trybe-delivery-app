cd3ec2f25bd53a7b157496b7cb31ff2ef7a6866b
Joao-Ortega <joao_ortega17@hotmail.com>
2022-06-10 18:37:46 -0300

    Fixing date

--- front-end/src/Components/SalesCard.jsx
+++ front-end/src/Components/SalesCard.jsx
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect } from 'react';
-import DeliveryContext from '../Context/DeliveryContext';
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import DeliveryContext from "../Context/DeliveryContext";
 
 export default function SalesCard() {
   const { fetchSales, sales } = useContext(DeliveryContext);
@@ -27,34 +28,59 @@ export default function SalesCard() {
     getInfos();
   }, []);
 
+  const treatDate = (date, id) => {
+    const noon = 12;
+    const rightFormat = new Date(date);
+    const dd = rightFormat.getDate();
+    const mm = rightFormat.getMonth() + 1;
+    const yyyy = rightFormat.getFullYear();
+    const hh = rightFormat.getHours();
+    const min = rightFormat.getMinutes();
+    const sec = rightFormat.getSeconds();
+    return (
+      <div className="date">
+        <span data-testid={ `customer_orders__element-order-date-${id}` }>
+          { `${dd}/${mm}/${yyyy}` }
+        </span>
+        <span>
+          { `${hh}:${min}:${sec}${hh >= noon ? 'PM' : 'AM'}` }
+        </span>
+      </div>
+    );
+  };
+
   return (
     <div>
       <main className="sales-container">
-        { sales.map(({ id, totalPrice, status, saleDate }) => (
-          <div key={ id } className="sale-card">
-            <div className="order">
-              <span>Pedido</span>
-              <span data-testid={ `customer_orders__element-order-id-${id}` }>
-                { id }
-              </span>
+        {sales.map(({ id, totalPrice, status, saleDate }) => (
+          <Link
+            key={ id }
+            to={ `/customer/orders/${id}` }
+            style={ { textDecoration: 'none', color: 'black' } }
+          >
+            <div className="sale-card">
+              <div className="order">
+                <span>Pedido</span>
+                <span data-testid={ `customer_orders__element-order-id-${id}` }>
+                  {id}
+                </span>
+              </div>
+              {/* { console.log(new Date(saleDate).getDate()) } */}
+              <div
+                className="status"
+                data-testid={ `customer_orders__element-delivery-status-${id}` }
+              >
+                {status}
+              </div>
+              <div className="date-and-price">
+                { treatDate(saleDate, id) }
+                <span data-testid={ `customer_orders__element-card-price-${id}` }>
+                  {totalPrice}
+                </span>
+              </div>
             </div>
-            {/* { console.log(new Date(saleDate).toLocaleString().split(' ')) } */}
-            <div
-              className="status"
-              data-testid={ `customer_orders__element-delivery-status-${id}` }
-            >
-              {status}
-            </div>
-            <div className="date-and-price">
-              <span data-testid={ `customer_orders__element-order-date-${id}` }>
-                {new Date(saleDate).toLocaleString()}
-              </span>
-              <span data-testid={ `customer_orders__element-card-price-${id}` }>
-                {totalPrice}
-              </span>
-            </div>
-          </div>
-        )) }
+          </Link>
+        ))}
       </main>
     </div>
   );
