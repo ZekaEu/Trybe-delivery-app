fcb04e26f1f1b1921fc75d8ca29eb70564af2213
Thiago <thiago_ssfr@hotmail.com>
2022-06-09 21:50:09 -0300

    Ajustes: Cors, Post to /sales

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,11 +3,13 @@ const productRouter = require('./routes/Product');
 const UserRouter = require('./routes/User');
 const SaleRouter = require('./routes/Sale');
 const errorMiddleware = require('./middlewares/errorMiddleware');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public')); 
+app.use(cors());
 app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
