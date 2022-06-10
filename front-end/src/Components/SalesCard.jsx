1e054b1e3185c7f9aa7417c7de35be4edfb545b2
Joao-Ortega <joao_ortega17@hotmail.com>
2022-06-10 18:43:03 -0300

    Fixing Lint

--- front-end/src/Components/SalesCard.jsx
+++ front-end/src/Components/SalesCard.jsx
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import DeliveryContext from "../Context/DeliveryContext";
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import DeliveryContext from '../Context/DeliveryContext';
 
 export default function SalesCard() {
   const { fetchSales, sales } = useContext(DeliveryContext);
